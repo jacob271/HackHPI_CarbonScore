ba5be58c3dcd9a1fb03090ce1bbb6eca9a49d499
Jacob Schäfer <js.jacobschaefer@gmail.com>
2022-03-20 08:39:19 +0100

    add product red jacket

--- src/app/product/types/category.enum.ts
+++ src/app/product/types/category.enum.ts
@@ -1,3 +1,4 @@
 export enum ProductCategory {
   Cookies = 'Cookies',
+  Clothing = 'Clothing',
 }
--- src/app/product/types/products.ts
+++ src/app/product/types/products.ts
@@ -1,6 +1,6 @@
-import { ProductCategory } from './category.enum'
-import { ProductScore } from './product-score.enum'
-import { Product } from './product.interface'
+import {ProductCategory} from './category.enum'
+import {ProductScore} from './product-score.enum'
+import {Product} from './product.interface'
 
 export const products: Product[] = [
   {
@@ -11,4 +11,12 @@ export const products: Product[] = [
     score: ProductScore.A,
     companyName: 'Cookie Corp.',
   },
+  {
+    id: 2,
+    category: ProductCategory.Clothing,
+    name: 'Red Jacket',
+    imageUrl: 'https://ae01.alicdn.com/kf/HTB1T5gYbiHrK1Rjy0Flq6AsaFXat/High-Quality-2019-Winter-Jacket-Men-Hooded-Windbreaker-and-Waterproof-Thick-Warm-Parka-Coat-Men-Casual.jpg',
+    score: ProductScore.C,
+    companyName: 'The ultimate jacket company'
+  }
 ]
