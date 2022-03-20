d37072323100d4e8c5e920bba5952022cc1f2aac
Daniel Kiss <kissd621@gmail.com>
2022-03-20 02:22:25 +0100

    temp2

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -3,9 +3,17 @@ import { TuiAvatarModule } from '@taiga-ui/kit'
 import { CoreModule } from '../core/core.module'
 import { ProductDetailsComponent } from './product-details/product-details.component'
 import { ProductRoutingModule } from './product-routing.module'
+import { ProductDetailsCarbonHistoryComponent } from './product-details/product-details-carbon-history/product-details-carbon-history.component'
+import { ProductDetailsCarbonFootprintIndicatorComponent } from './product-details/product-details-carbon-footprint-indicator/product-details-carbon-footprint-indicator.component';
+import { ProductDetailsCarbonScoreComponent } from './product-details/product-details-carbon-score/product-details-carbon-score.component'
 
 @NgModule({
-  declarations: [ProductDetailsComponent],
+  declarations: [
+    ProductDetailsComponent,
+    ProductDetailsCarbonHistoryComponent,
+    ProductDetailsCarbonFootprintIndicatorComponent,
+    ProductDetailsCarbonScoreComponent,
+  ],
   imports: [CoreModule, ProductRoutingModule, TuiAvatarModule],
   exports: [],
 })
