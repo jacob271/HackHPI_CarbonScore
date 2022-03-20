0f2399bce0fec54076e069f05912a23fe143aa73
Daniel Kiss <kissd621@gmail.com>
2022-03-20 11:12:31 +0100

    temp10

--- src/app/search/search-results/search-results.component.ts
+++ src/app/search/search-results/search-results.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 import { map, Observable } from 'rxjs'
 import { products } from 'src/app/product/types/products'
+import { SearchResult } from 'src/app/product/types/search-result.interface'
 
 @Component({
   selector: 'app-search-results',
@@ -10,6 +11,10 @@ import { products } from 'src/app/product/types/products'
 })
 export class SearchResultsComponent {
   readonly query$: Observable<string> = this.route.paramMap.pipe(map((paramMap) => paramMap.get('searchQuery')))
+
+  readonly filteredSearchResults$: Observable<SearchResult[]> = this.query$.pipe(
+    map((query) => this.searchResults.filter((searchResult) => searchResult.name.toLowerCase().includes(query)))
+  )
 
   readonly searchResults = products
 
