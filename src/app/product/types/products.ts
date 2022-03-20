6fb2854041e72fc212c1e78ca200ae8fb88b603e
Weitspringer <luca_springer@yahoo.com>
2022-03-20 11:49:15 +0100

    Add new products and category

--- src/app/product/types/products.ts
+++ src/app/product/types/products.ts
@@ -18,5 +18,37 @@ export const products: Product[] = [
     imageUrl: 'https://ae01.alicdn.com/kf/HTB1T5gYbiHrK1Rjy0Flq6AsaFXat/High-Quality-2019-Winter-Jacket-Men-Hooded-Windbreaker-and-Waterproof-Thick-Warm-Parka-Coat-Men-Casual.jpg',
     score: ProductScore.C,
     companyName: 'H&M'
+  },
+  {
+    id: 3,
+    category: ProductCategory.Clothing,
+    name: 'Jacket "Eco"',
+    imageUrl: 'https://tse2.mm.bing.net/th?id=OIP.lAUExyxT6_Ia05xj-gJ36wHaKa&pid=Api',
+    score: ProductScore.B,
+    companyName: 'Marc O\'Polo'
+  },
+  {
+    id: 4,
+    category: ProductCategory.Clothing,
+    name: 'Recycled Jacket',
+    imageUrl: 'https://tse3.mm.bing.net/th?id=OIP.gQqWX7kR_2DFtehjj_x3RAHaLH&pid=Api',
+    score: ProductScore.A,
+    companyName: 'MJ Wear'
+  },
+  {
+    id: 5,
+    category: ProductCategory.Bicycle,
+    name: 'Bamboo Bike',
+    imageUrl: 'https://tse3.mm.bing.net/th?id=OIP.6VuUNbKR5rWvqAd-O7ZcZQHaGF&pid=Api',
+    score: ProductScore.B,
+    companyName: 'My Boo'
+  },
+  {
+    id: 6,
+    category: ProductCategory.Bicycle,
+    name: 'Racing Bike',
+    imageUrl: 'https://tse4.mm.bing.net/th?id=OIP.jm2WbSXNz1DO2C4RY-dcogHaEr&pid=Api',
+    score: ProductScore.C,
+    companyName: 'Adamant'
   }
 ]
