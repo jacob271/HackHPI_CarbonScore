e4869b16c56cbd7cb142fc3cb09ff4510e0d6e0e
Daniel Kiss <kissd621@gmail.com>
2022-03-20 11:06:41 +0100

    temp9

--- src/app/product/product-details/product-details-ingredients/product-details-ingredients.component.ts
+++ src/app/product/product-details/product-details-ingredients/product-details-ingredients.component.ts
@@ -31,7 +31,7 @@ root\\palmoil,"Palm Oil",5,16,15
     tree.sum(function (d: any) {
       return d.value
     })
-    const width = 500,
+    const width = 1000,
       height = 300
     const treemap = d3.treemap().size([width, height]).round(true).padding(1)
 
@@ -69,6 +69,23 @@ root\\palmoil,"Palm Oil",5,16,15
           return '#d3d78c'
         }
       })
+    svg
+      .selectAll('text')
+      .data(tree.leaves())
+      .enter()
+      .append('text')
+      .attr('x', function (d: any) {
+        return d.x0 + 10
+      }) // +10 to adjust position (more right)
+      .attr('y', function (d: any) {
+        return d.y0 + 20
+      }) // +20 to adjust position (lower)
+      .text(function (d: any) {
+        return d.data.name
+      })
+      .attr('font-size', '15px')
+      .attr('fill', 'black')
+
     cell.append('title').text(function (d: any) {
       if (parseFloat(d.data.thisaverage) > parseFloat(d.data.average)) {
         return d.data.name + '\n' + d.value + ' average CO2e per unit [kg]\nMore than average :('
